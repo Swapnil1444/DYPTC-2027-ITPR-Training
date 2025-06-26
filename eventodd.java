5b796688217bc74cf8a0c900fcace22a4fd28f4f
Swapnil Supekar <swpnilsupekar@gmail.com>
2025-06-26 13:25:19 +0530

    Update eventodd.java

--- eventodd.java
+++ eventodd.java
@@ -1,7 +1,9 @@
 public class eventodd{
   public static void main(String[]args){
     for(int i=0; i<=10;i++){
-      System.out.println(i);
+      if(i%2==0){
+       System.out.println(i);
+      }
     }
         
   }
