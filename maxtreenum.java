072651d1a3d46d5eb0b31e21188f9210850a3b29
Swapnil Supekar <swpnilsupekar@gmail.com>
2025-06-26 13:41:42 +0530

    Update maxtreenum.java

--- maxtreenum.java
+++ maxtreenum.java
@@ -1,5 +1,13 @@
-public class maxtreenum{
-  public static void main(String[]args){
-    
-  }
+import java.util.Scanner;
+
+public class maxofthree {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter three numbers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int max = (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c);
+        System.out.println("Maximum number is: " + max);
+    }
 }
